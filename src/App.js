a0d492d237c1f139c448f76f16ae45c9b902b228
Adil <adil1350@gmail.com>
2019-04-14 23:41:51 +0100

    edited readme & added local logo file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Loader from './Loader';
+import logo from './logo.jpg';
 
 const App = () => {
 	const [results, setResults] = useState([]);
@@ -17,9 +18,7 @@ const App = () => {
 	const getResults = async () => {
 		setLoading(true);
 		try {
-			const res = await axios.get(
-				`http://hn.algolia.com/api/v1/search?query=${query}`
-			);
+			const res = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
 			setResults(res.data.hits);
 		} catch (err) {
 			setError(err);
@@ -27,7 +26,7 @@ const App = () => {
 		setLoading(false);
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmit = e => {
 		e.preventDefault();
 		getResults();
 	};
@@ -39,24 +38,20 @@ const App = () => {
 
 	return (
 		<div className="container max-w-md mx-auto p-4 m-2 bg-green-dark shadow-lg rounded">
-			<img src="https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/61/e7/6d/61e76d36-7b44-7173-2540-412605060935/AppIcon-1x_U007emarketing-85-220-0-6.png/246x0w.jpg" alt="Hacker News" className="float-right h-16" />
+			<img src={logo} alt="Hacker News" className="float-right h-16" />
 			<h1 className="text-grey-darkest font-thin">Hackernews Quick Search</h1>
 			<form className="mb-5" onSubmit={handleSubmit}>
 				<input
 					type="text"
 					value={query}
-					onChange={(e) => setQuery(e.target.value)}
+					onChange={e => setQuery(e.target.value)}
 					ref={searchInputRef}
 					className="border p-1 rounded"
 				/>
 				<button type="submit" className="bg-red-light rounded m-1 p-1">
 					Search
 				</button>
-				<button
-					type="button"
-					onClick={handleClearSearch}
-					className="bg-grey text-white p-1 rounded"
-				>
+				<button type="button" onClick={handleClearSearch} className="bg-grey text-white p-1 rounded">
 					Clear
 				</button>
 			</form>
@@ -64,7 +59,7 @@ const App = () => {
 				<Loader />
 			) : (
 				<ul className="list-reset leading-loose">
-					{results.map((result) => (
+					{results.map(result => (
 						<li key={result.objectID}>
 							<a
 								href={result.url}
